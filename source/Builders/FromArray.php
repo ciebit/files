ab2eafa9c7814a8fc68ff95a5eb03176085e82db
Maike <maike_jbe@hotmail.com>
2018-08-03 08:58:50 -0300

    Adicionada lógica de construção do objeto File

--- source/Builders/FromArray.php
+++ source/Builders/FromArray.php
@@ -3,10 +3,12 @@ declare(strict_types=1);
 
 namespace Ciebit\Files\Builders;
 
-use Ciebit\Files\Builders\Build;
+use Ciebit\Files\Builders\Builder;
 use Ciebit\Files\File;
+use Ciebit\Files\Status;
+use DateTime;
 
-abstract class FromArray implements Build
+class FromArray implements Builder
 {
     private $data; #:array
 
@@ -18,6 +20,32 @@ abstract class FromArray implements Build
 
     public function build(): File
     {
-
+        if (
+            ! is_array($this->data) OR
+            ! isset($this->data['uri'])
+        ) {
+            throw new Exception('ciebit.files.builders.invalid', 3);
+        }
+        $file = new File(
+            $this->data['name'],
+            $this->data['uri'],
+            $this->data['mimetype'],
+            Status::DRAFT()
+        );
+        isset($this->data['date_hour'])
+        && $file->setDateHour(new DateTime($this->data['date_hour']));
+        isset($this->data['id'])
+        && $file->setId((int) $this->data['id']);
+        isset($this->data['extension'])
+        && $file->setExtension((string) $this->data['extension']);
+        isset($this->data['mimetype'])
+        && $file->setMimetype((string) $this->data['mimetype']);
+        isset($this->data['size'])
+        && $file->setSize((float) $this->data['size']);
+        isset($this->data['views'])
+        && $file->setViews((int) $this->data['views']);
+        isset($this->data['status'])
+        && $file->setStatus(new Status((int) $this->data['status']));
+        return $file;
     }
 }
