e89f90d5b69579fe12e8f55d7c4ba032ea5f7606
Maike <maike_jbe@hotmail.com>
2018-08-07 08:47:30 -0300

    Criação de classe

---
+++ source/Unknown/Unknown.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Ciebit\Files\Unknown;
+
+use Ciebit\Files\Status;
+use DateTime;
+
+class Unknown extends File
+{
+    public function __construct(
+        string $name,
+        string $uri,
+        string $mimetype,
+        Status $status
+    ) {
+        parent::__construct($name, $uri, $mimetype, $status);
+    }
+}
