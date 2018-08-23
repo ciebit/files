d6b323401f3944dc4378e7983d8ec6e6b2d1225e
Maike <maike_jbe@hotmail.com>
2018-08-23 11:32:08 -0300

    Criação de classe

---
+++ source/Pdfs/Pdf.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Ciebit\Files\Pdfs;
+
+use Ciebit\Files\File;
+use Ciebit\Files\Status;
+
+class Pdf extends File
+{
+    public function __construct(
+        string $name,
+        string $mimetype,
+        string $uri,
+        status $status
+    ) {
+        parent::__construct($name, $uri, $mimetype, $status);
+    }
+}
