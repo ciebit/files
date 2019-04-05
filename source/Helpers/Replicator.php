3ad75942bd07d5eb05612ec435111deb67ce52c5
Johnny Saymon <saymon@ciebit.com.br>
2019-04-05 12:14:38 -0300

    Criacao de classe para replicacao de objetos Files

---
+++ source/Helpers/Replicator.php
@@ -0,0 +1,19 @@
+<?php
+namespace Ciebit\Files\Helpers;
+
+use Ciebit\Files\Builders\Context;
+use Ciebit\Files\File;
+
+use function array_merge;
+use function json_decode;
+use function json_encode;
+
+class Replicator
+{
+    public function replicate(File $file, array $variations = []): File
+    {
+        $data = json_decode(json_encode($file), true);
+        $data = array_merge($data, $variations);
+        return (new Context)->setData($data)->buid();
+    }
+}
